13012c52ac1e7c25324e428910f65ce3a424883e
Dave <Dave.Troupe@gmail.com>
2017-01-19 19:23:14 -0500

    Initial commit

---
+++ src/RecursiveFibonacci.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Dave on 1/19/17.
+ */
+
+class RecursiveFibonacci {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 15) {
+            System.out.print(fibonacci(x) + " ");
+            x++;
+        }
+
+    }
+
+    private static int fibonacci(int n) {
+
+        // base case(s) n = 0 or 1
+        if (n == 0)
+            return 0;
+
+        else if (n== 1)
+            return 1;
+
+        else
+            //recursive call
+            return fibonacci(n-1) + fibonacci(n - 2);
+    }
+}
